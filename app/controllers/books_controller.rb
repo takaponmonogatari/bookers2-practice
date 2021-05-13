412e6af26b0e240c9d214b03d5979679608ddcc2
takaponmonogatari <taka11hiro26@gmail.com>
2021-05-13 10:52:02 +0000

    [Update] books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,13 @@
 class BooksController < ApplicationController
+    before_action :authenticate_user!
+    before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+    
     def index
+        @books = Book.all
+        @book = Book.new
     end
     def show
+        
     end
     def edit
     end
